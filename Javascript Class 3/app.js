6022b08c122d8d5375bd2a89ebe2e994f1facacc
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-08 16:53:56 +0500

    Javascript Class 3
    Javascript Class 3

---
+++ Javascript Class 3/app.js
@@ -0,0 +1,25 @@
+// alert("Thanks for your input!");
+
+// var message = "Thanks for your input!";
+// alert(message);
+// alert("Thanks, " + userName + "!");
+
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + userName + banger);
+
+// message = "Thanks, ";
+// userName = "Susan";
+// banger = "!";
+// customMess = message + userName + banger;
+// alert(customMess);
+
+// alert("2" + "2");
+// alert("2 plus 2 equals " + 2 + 2);
+
+// var spec = prompt("Your species?", "human");
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+
