b9ab917eef32c88639a0886bb3760e5a7d786b8a
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-08 16:53:30 +0500

    Javascript Class 2
    Javascript Class 2

---
+++ Javascript Class 2/app.js
@@ -0,0 +1,19 @@
+// var popularNumber = 2 + 2;
+// alert(popularNumber);
+// alert(2 + 2)
+// var num = 10;
+// var popularNumber = num + 200;
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// num = num + 1;
+// num++;
+// num--;
+// var num = 1;
+// var newNum = num++;
+// var num = 1;
+// var newNum = ++num;
+// var num = 1;
+// var newNum = --num;
+
+
