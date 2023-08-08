b5d2dcd58ace88ab72162175204000888089f716
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-08 16:54:20 +0500

    Javascript Class 4
    Javascript Class 4

---
+++ Javascript Class 4/app.js
@@ -0,0 +1,30 @@
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+
+// var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  score++;
+//  userIQ = "genius";
+// alert("Correct!");
+//  }
+
+//  if (yourTicketNumber !== 487208) {
+//  alert("Better luck next time.");
+//  }
+
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  else {
+//  alert("Wrong answer");
+//  }
+
+// var number = 10
+// if (number >= 5) {
+//     alert("hello")
+// } else {
+//     alert("world")
+// }
+
