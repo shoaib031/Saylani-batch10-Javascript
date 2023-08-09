8ad7c6459c46b9c40d6248be070afceee5e0d7c4
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-09 21:43:43 +0500

    Javascript Class 5
    Javascript Class 5

---
+++ javascript Class 5/app.js
@@ -0,0 +1,105 @@
+// Question 1: Write an if statement that checks if a variable age is greater than or equal to 18. If true, display "You are an adult,"
+// otherwise display "You are a minor."
+
+// var input = prompt("Enter your age")
+// if(input >= 18){
+// document.write("You are adult")
+// }else{
+//     document.write("You are minor")
+// }
+
+// Question 2: Ask the user to enter their name using prompt. If the name is "John," display "Hello, John!" Otherwise, display
+// "Hello, stranger!"
+
+// var name = prompt("Enter your name")
+// if (name >= john) {
+//     document.write("hello john")
+// } else {
+//     document.write("hello stranger")
+
+// }
+
+// Question 3: Calculate the area of a rectangle using the formula area = length * width. Ask the user to enter the length and
+// width using prompt, and then display the result.
+
+// var length = prompt("length")
+// var width = prompt("width")
+// var result = length * width
+// var confirm = result; 
+
+// Question 4: Create a simple calculator. Ask the user to enter two numbers and an arithmetic operator (+, -, *, /). Perform the
+// corresponding operation and display the result.
+
+
+// Question 5: Ask the user to enter their age using prompt. If the age is between 13 and 19 (inclusive), display "You are a
+// teenager." Otherwise, display "You are not a teenager."
+
+// var age = prompt("Enter your age")
+// if(age >= 13 && age <= 19){
+// document.write("You are teenager")
+// }else{
+//     document.write("You are not a teenager")
+// }
+
+// Question 6: Calculate the volume of a cylinder using the formula volume = π * radius^2 * height. Ask the user to enter the
+// radius and height using prompt, and then display the result.
+
+// Question 7: Ask the user to enter a number using prompt. Check if the number is positive, negative, or zero, and display the
+// corresponding message.
+
+// var number = prompt("Enter the number ")
+// if(number === 2){
+// document.write("it is even")
+// }else{
+//     document.write("it is odd")
+// }
+
+// Question 8: Ask the user to enter a grade (A, B, C, D, or F) using prompt. Display a message indicating if they passed (grade
+    // is A, B, C, or D) or failed (grade is F).
+
+// var grade = prompt("Enter your grade")
+// if(grade >= 90 && grade <= 100){
+// document.write("A+")
+// }else if(grade >= 80 && grade <= 90){
+//     document.write("A")
+// }else if(grade >= 70 && grade <= 80){
+//     document.write("b")
+
+// }else{
+// document.write("you are fail")
+// }
+
+// Question 9: Ask the user to enter three numbers using prompt. Find the maximum and minimum of the three numbers and
+// display the results.
+
+// Question 10: Create a program that converts temperatures between Celsius and Fahrenheit. Ask the user to enter the
+// temperature and the unit (C or F), then perform the conversion and display the result.
+
+// var centigtadeTemp = 12;
+// var farenTemp = 68;
+
+// var centitofaren = (farenTemp - 32) * 5 / 9;
+// var farentocenti = (centigtadeTemp * 9 / 5) + 32;
+
+// alert(farentocenti);
+// alert(centitofaren);
+
+// Question 12: Create a simple discount calculator. Ask the user to enter the original price and the discount percentage using
+// prompt. Calculate the discounted price and display the result.
+
+// var original = prompt("enter price")
+// var discount = prompt("enter discount price")
+// var calculate = (original*discount) / 100
+// var price = original - discount
+// document.write(price)
+
+// Question 14: Ask the user to enter their name using prompt. Check if the name starts with the letter "A" (uppercase or
+    // lowercase) and display a message accordingly.
+
+//     var letter = prompt("enter a letter")
+//     if(letter === A){
+// document.write("A")
+//     }else{
+//         document.write("no")
+//     }
+
