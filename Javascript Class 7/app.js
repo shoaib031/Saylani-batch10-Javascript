d30b50378dedc5c55550befd485da620e15e74ef
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-16 14:39:49 +0500

    Javascript CLass 7
    Javascript Class 7

---
+++ Javascript Class 7/app.js
@@ -0,0 +1,73 @@
+// 1 Given the array fruits = ["apple", "banana", "orange"], use the push method to add "grape" to the end of the array.
+
+// var fruits = ["apple", "banana", "orange"]
+// fruits.push("grape")
+// console.log(fruits)
+
+// 2 Using the array numbers = [1, 2, 3, 4, 5], use the pop method to remove the last element from the array.
+
+// var numbers = [1, 2, 3, 4, 5]
+// numbers.pop()
+// console.log(numbers)
+
+// 3 Given the array colors = ["red", "green", "blue"], use the unshift method to add "yellow" to the beginning of the array.
+
+// var colors = ["red", "green", "blue"] 
+// colors.unshift("yellow")
+
+// 4 Using the array days = ["Monday", "Tuesday", "Wednesday", "Thursday"], use the shift method to remove the first element from the array.
+
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+// days.shift()
+
+// 5 Create an empty array called names. Use the push method to add three names: "Alice", "Bob", and "Charlie".
+
+// var empty = []
+// empty.push("Shoaib", "Bilal", "Raza")
+// console.log(empty)
+
+// 6 Using the array numbers = [10, 20, 30, 40, 50], use a for loop to calculate the sum of all the numbers in the array.
+
+// var addition = [10, 20, 30, 40, 50]
+// for(i = 0; addition < 10; i++){
+// console.log(addition)
+// }
+
+// 7 Create an array called evenNumbers and use a for loop to populate it with even numbers from 2 to 20
+
+// var evenNumbers = [];
+// for(var i = 2; i <= 20; i++){
+// if(i % 2 == 0){
+//     document.write(i + "<br>")
+// }
+// }
+
+// 8 Given the array fruits = ["apple", "banana", "orange", "kiwi"], use a for loop to print each fruit on a separate line.
+
+// var separete = ["apple", "banana", "orange", "kiwi"]
+// for(var i = 0; i <= separete.length; i++){
+//     document.write(separete[i] + "<br>")
+// }  
+
+// 9 Using the array numbers = [5, 10, 15, 20, 25], use a for loop to calculate the total of all the numbers in the array.
+
+// const numbers = [5, 10, 15, 20, 25];
+// let total = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     total += numbers[i];
+// }
+// console.log("The total is:", total);
+
+// 10 Using the array animals = ["cat", "dog", "elephant", "giraffe"], use a for loop to find and print the index of the "elephant" in the array.
+
+// var animals = ["cat", "dog", "elephant", "giraffe"]
+// console.log(animals.indexOf("elephant"))
+
+// 11 Create an array called oddNumbers and use a for loop to populate it with odd numbers from 1 to 15
+
+// var oddNumbers = [];
+// for(var i = 1; i <= 15; i++){
+//     if(i % 3 == 0){
+// document.write(i + "<br>" )
+//     }
+// }
