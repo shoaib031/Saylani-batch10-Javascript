46a16d37cdd04043c8cff34a5eb45bd9d254957d
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-08-30 15:59:15 +0500

    Javascript Class 8
    Javascript Class 8

---
+++ Javascript Class 8/app.js
@@ -0,0 +1,49 @@
+// Given the array numbers = [1, 2, 3, 4, 5], use the splice method to remove the second element (2) from the array. 
+
+// var numbers = [1, 2, 3, 4, 5]
+// numbers.splice(1,1)
+// console.log(numbers)
+
+// Using the array fruits = ["apple", "banana", "orange", "kiwi", "grape"], use the splice method to replace "orange" and "kiwi"
+// with "strawberry" and "pineapple".
+
+// var arr = ["apple", "banana", "orange", "kiwi", "grape"]
+// arr.splice(2, 3, "strawberry", "pineapple")
+// console.log(arr)
+
+// Given the array colors = ["red", "green", "blue", "yellow", "purple"], use the slice method to create a new array containing only
+// "green" and "blue"
+
+// var colors = ["red", "green", "blue", "yellow", "purple"]
+// var result = colors.slice(0,3,4)
+// console.log(result)
+
+// Using the array numbers = [10, 20, 30, 40, 50], use the slice method to create a new array containing the last three elements.
+
+// var numbers = [10, 20, 30, 40, 50]
+// var result = numbers.slice(0,1)
+// console.log(result)
+
+//15 Create an array called grades with five test scores. Use a for loop to find and print the lowest grade in the array.
+
+// var grades = ""
+// for(var i = 1; i <= 5; i++){   
+// document.write("*" + "<br>")
+// }
+
+// Using the array numbers = [3, 6, 9, 12, 15], use a for loop to calculate and print the sum of the array's elements
+
+// Create an array called colors with various color names. Use a for loop to print each color on a separate line.
+
+// var colors = ["red", "green", "yellow", "purple"]
+// for(i = 0; i < colors.length; i++ ){
+//     document.write(colors[i] + "<br>")
+// }
+
+// Given the array prices = [25, 30, 15, 10, 50], use a for loop to find and print the index of the highest price.
+
+// var prices = [25, 30, 15, 10, 50]
+// for(var i = 0; i <= prices.length-5; i++){
+// document.write(prices.indexOf(50) + "<br>")
+// }
+
