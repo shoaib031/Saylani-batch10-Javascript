af7a39ba84cc5a664cfefa64160263f728cb1cf4
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-09-27 11:38:00 +0500

    Javascript Class 13
    Javascript Class 13

---
+++ Javascript Class 13/app.js
@@ -0,0 +1,24 @@
+// let car =  {
+//     color : "Black", 
+//     model : "2023",
+//     showCarName: function(){
+//     alert("swift")
+//     return "swift"
+//     }
+// }
+// function greetings(){
+//     alert("Greetings")
+// }
+// console.log("car ->", greetings)
+// console.log(n)
+
+// Current Date Display:
+// Create a webpage that displays the current date when it loads. (Hint: Use the Date object and the innerHTML property
+// to display the date.)
+
+// var date = new Date()
+// console.log(date)
+
+// Birthday Calculator:
+// Develop a script that takes a birthdate from the user and calculates their age. Display the age on the webpage.
+
